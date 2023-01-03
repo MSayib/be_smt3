a0ce84a2ab5861f63d6677a147698d36a44701b2
Muhamad Sayib Roziq <sayib@MacbookPro.local>
2023-01-03 23:56:42 +0700

    push tugas 13

--- Task Pertemuan 13/routes/api.js
+++ Task Pertemuan 13/routes/api.js
@@ -13,5 +13,8 @@ router.post("/students", StudentController.store);
 router.put("/students/:id", StudentController.update);
 router.delete("/students/:id", StudentController.destroy);
 
+//NEW Task 13
+router.get("/students/:id", StudentController.show);
+
 // Export router
 module.exports = router;
--- Task Pertemuan-11/data/students.js
+++
@@ -1,5 +0,0 @@
-// TODO 1: Buat data students
-var students = ["Sayib", "Fikri", "Fahmi", "Asnur", "Piscki", "Angga"];
-
-// TODO 2: export data students
-module.exports = students;
